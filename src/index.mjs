45cc98b1924ef030ef062d804320c9928c24368d
Victor Martinez <105192432+Victor-FMR@users.noreply.github.com>
2026-05-07 01:36:08 +0000

    cambio

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,3 @@
-import "./styles.css";
-import "./home.js";
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
 //const domain = (window.location.hostname = "webflow.io");
 
 // const h = document.createElement("script");

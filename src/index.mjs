1e1878f475ef55bf0c3fc24a94dd8ccd534bd438
Victor Martinez <105192432+Victor-FMR@users.noreply.github.com>
2026-05-07 01:27:21 +0000

    cambio de color

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,10 @@ document.getElementById("app").innerHTML = `
 `;
 //const domain = (window.location.hostname = "webflow.io");
 
-const h = document.createElement("script");
-h.src = "https://supreme-elite-chosen-ones.webflow.io/";
-document.head.appendChild(h);
+// const h = document.createElement("script");
+// h.src = "https://supreme-elite-chosen-ones.webflow.io/";
+// document.head.appendChild(h);
+
+const body = document.getElementById("particlesjs");
+
+body.style.backgroundColor = "red";

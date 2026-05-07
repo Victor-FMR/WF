9e140419fa3ee8c40d99ec36baac064a5a849c68
Victor Martinez <105192432+Victor-FMR@users.noreply.github.com>
2026-05-07 05:47:18 +0000

    "cambio"

--- src/index.mjs
+++
@@ -1,9 +0,0 @@
-//const domain = (window.location.hostname = "webflow.io");
-
-// const h = document.createElement("script");
-// h.src = "https://supreme-elite-chosen-ones.webflow.io/";
-// document.head.appendChild(h);
-
-const body = document.getElementById("particlesjs");
-
-body.style.backgroundColor = "red";

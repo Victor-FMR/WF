c164aec423fc47c4e44b64d423b99a0a9e775314
Victor Martinez <105192432+Victor-FMR@users.noreply.github.com>
2026-05-07 02:57:04 +0000

    rename index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//const domain = (window.location.hostname = "webflow.io");
+
+// const h = document.createElement("script");
+// h.src = "https://supreme-elite-chosen-ones.webflow.io/";
+// document.head.appendChild(h);
+
+const body = document.getElementById("particlesjs");
+
+body.style.backgroundColor = "red";

3089ffcdcd7147ef6a44fd1142484f9a8cec0b0d
Victor Martinez <105192432+Victor-FMR@users.noreply.github.com>
2026-05-08 02:42:21 +0000

    cambio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-//const domain = (window.location.hostname = "webflow.io");
-
-// const h = document.createElement("script");
-// h.src = "https://supreme-elite-chosen-ones.webflow.io/";
-// document.head.appendChild(h);
-
 const body = document.getElementById("particlesjs");
 
-body.style.backgroundColor = "white";
+body.style.backgroundColor = "purple";
